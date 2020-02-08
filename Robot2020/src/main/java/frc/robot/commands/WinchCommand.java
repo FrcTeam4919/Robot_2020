a34c69ab7b73b9e7bd60fcbc682212a9957b141f
HunterPaul4919 <47090143+HunterPaul4919@users.noreply.github.com>
2020-02-08 15:41:51 -0800

    Began mapping out the motor control buttons

--- Robot2020/src/main/java/frc/robot/commands/WinchCommand.java
+++ Robot2020/src/main/java/frc/robot/commands/WinchCommand.java
@@ -10,8 +10,9 @@
 
 
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
+import frc.robot.*;
 
 /**
  *
@@ -37,6 +38,7 @@ public class WinchCommand extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        
     }
 
     // Called repeatedly when this Command is scheduled to run

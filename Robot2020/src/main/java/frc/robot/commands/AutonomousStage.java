e1722e2258ed863017770ac28deba0576e1e459a
THA6 <maplebar.11.06@gmail.com>
2020-02-17 13:12:27 -0800

    Update AutonomousStage.java

--- Robot2020/src/main/java/frc/robot/commands/AutonomousStage.java
+++ Robot2020/src/main/java/frc/robot/commands/AutonomousStage.java
@@ -1,12 +1,12 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.*;
 
 public class AutonomousStage extends CommandGroup{
     public AutonomousStage(){
         addSequential(new TimedDrivetrain(3));
         addSequential(new AutoTurn(2));
-        addParallel(new AutoConveyor(3));
+        addParallel(new Command(3));
         addParallel(new TimedBallLoader(3));
         addParallel(new AutoShooter(3));
     }
